1c4057007b815a4dd65edd50704b3b5cffab000d
Arpit Singh <singharpit0911@gmail.com>
2024-10-14 13:30:22 +0530

    update route

--- wonderlust-airbnb/app.js
+++ wonderlust-airbnb/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const { render } = require("ejs");
+const methodOverride = require("method-override");
 
 //  * use declaration
 const app = express();
@@ -23,6 +23,7 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // * Routes
 app.get("/", (req, res) => {
@@ -68,6 +69,21 @@ app.post("/listings", async (req, res) => {
   const newListing = new Listing(listing);
   await newListing.save();
   res.redirect("/listings");
+});
+
+// * edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const foundListing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing: foundListing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  const { listing } = req.body;
+  const { id } = req.params;
+  console.log(listing);
+  await Listing.findByIdAndUpdate(id, listing);
+  res.redirect(`/listings/${id}`);
 });
 
 // * Express server connection
