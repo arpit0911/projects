63f23e36cb702789ac9b4538c9ddf68183df0d27
Arpit Singh <singharpit0911@gmail.com>
2024-12-26 13:42:39 +0530

    signup and login feature

--- wonderlust-airbnb/app.js
+++ wonderlust-airbnb/app.js
@@ -5,8 +5,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
@@ -67,18 +68,19 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/demouser", async (req, res) => {
-  const fakeUser = {
-    email: "student@gmail.com",
-    username: "delta-student",
-  };
+// app.get("/demouser", async (req, res) => {
+//   const fakeUser = {
+//     email: "student@gmail.com",
+//     username: "delta-student",
+//   };
 
-  let registeredUser = await User.register(fakeUser, "password");
-  res.send(registeredUser);
-});
+//   let registeredUser = await User.register(fakeUser, "password");
+//   res.send(registeredUser);
+// });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
---
+++ wonderlust-airbnb/routes/user.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+// get route
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+// register route
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+// show login route
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+// login route
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  wrapAsync(async (req, res) => {
+    req.flash("success", "welcome to wanderlust");
+    res.redirect("/listings");
+  })
+);
+
+module.exports = router;
